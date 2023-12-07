bac36cb649c01769904810dd21995fa721891d72
Yin Len Wong Mestas <91991394+yinwm88@users.noreply.github.com>
2023-12-07 15:48:20 -0600

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from menu import llamar_modalidad
-
-
-if __name__ == "__main__":
-
-    modalidad = input(" \nIngrese algunas de las siguientes opciones seguido de los argumentos necesarios según la modalidad elegida:\n \n'c' para cifrar. \n'd' para descifrar. \n's' para salir.\n")  
-    llamar_modalidad(modalidad)
