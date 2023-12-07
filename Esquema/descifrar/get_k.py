52dd7b3be21d3d139414e234f858012b8c45b945
Yin Len Wong Mestas <91991394+yinwm88@users.noreply.github.com>
2023-12-07 15:48:00 -0600

    Delete Esquema directory

--- Esquema/descifrar/get_k.py
+++
@@ -1,8 +0,0 @@
-from .algoritmo_lagrange import lagrange_finite_field
-
-
-primo = 208351617316091241234326746312124448251235562226470491514186331217050270460481
-
-
-def get_k(evaluaciones):
-    return lagrange_finite_field(primo, evaluaciones, 0)
